5bd4bbc25373efc427f747310327478a2bac8de0
Vladimir Ledovskih <vladimir.ledovskih@red-soft.ru>
2026-08-08 22:19:17 +0300

    Add condition waiting

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,3 +1,5 @@
+from selenium.webdriver.support import expected_conditions as EC
+
 from locators.login_page import LoginPageLocators
 from pages.base_page import BasePage
 
@@ -7,8 +9,11 @@ class LoginPage(BasePage, LoginPageLocators):
         super().__init__(driver)
 
     def login(self, username, password):
+        self.wait.until(EC.element_to_be_clickable(self.USERNAME_INPUT))
         self.driver.find_element(*self.USERNAME_INPUT).send_keys(username)
+        self.wait.until(EC.element_to_be_clickable(self.PASSWORD_INPUT))
         self.driver.find_element(*self.PASSWORD_INPUT).send_keys(password)
+        self.wait.until(EC.element_to_be_clickable(self.LOGIN_BUTTON))
         self.driver.find_element(*self.LOGIN_BUTTON).click()
 
         return self
