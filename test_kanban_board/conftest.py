74101d5dd8cd04ff016e0a593f14b298cd196316
Vladimir Ledovskih <vladimir.ledovskih@red-soft.ru>
2026-08-08 23:47:14 +0300

    Update Chrome options

--- test_kanban_board/conftest.py
+++ test_kanban_board/conftest.py
@@ -16,8 +16,11 @@ from pages.labels_page import LabelsPage
 @pytest.fixture
 def driver():
     options = Options()
-    options.add_argument("--headless")  # Запуск без окна браузера
-    options.add_argument("--start-fullscreen")
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
     driver = webdriver.Chrome(options=options)
     driver.get(APP_BASE_URL)
     WebDriverWait(driver, 10).until(EC.title_contains("Task manager"))
