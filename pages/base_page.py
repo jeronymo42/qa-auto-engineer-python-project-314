4bdf7d729feb2e18e3615d33c04dcf453c192203
Vladimir Ledovskih <vladimir.ledovskih@red-soft.ru>
2026-08-08 22:38:25 +0300

    Change congition

--- pages/base_page.py
+++ pages/base_page.py
@@ -94,7 +94,7 @@ class BasePage(HeaderMenuLocators, SideMenuLocators, BasePageLocators):
         return row
 
     def select_all_rows(self):
-        self.wait.until(EC.element_to_be_clickable(self.MAIN_CHECKBOX))
+        self.wait.until(EC.presence_of_element_located(self.MAIN_CHECKBOX))
         self.driver.find_element(*self.MAIN_CHECKBOX).click()
         return self
 
